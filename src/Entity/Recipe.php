5e57a4a0eba372e7ddb7a7a1f4dbaaefb4f86ca2
moonye@live.fr <moonye@live.fr>
2025-01-31 16:34:55 +0100

    MODIF : CSS et ajout image BBD

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -31,6 +31,9 @@ class Recipe
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
     public function __construct()
     {
         $this->createdAt = new DateTimeImmutable();
@@ -97,6 +100,18 @@ class Recipe
     public function setCategory(?Category $category): static
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,11 +6,13 @@ use App\Entity\Category;
 use App\Entity\Recipe;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class RecipeType extends AbstractType
 {
@@ -27,6 +29,17 @@ class RecipeType extends AbstractType
                     'placeholder' => 'Nom de la recette',
                 ],
             ])
+
+
+
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new Image()
+                ]
+            ])
+
+
 
             ->add('description', TextareaType::class, [
                 'label' => 'Description :',
